17375bf965f6155d5a7f4c8a66c3ab1052c51fbf
Parth Sharma <pxs4528@mavs.uta.edu>
2022-10-02 00:33:11 -0500

    1.2

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import Axios from 'axios';
 function App() {
   const [rocketName, setrocketName] = useState("");
   useEffect(() => {
+    Axios.get("https://api.spacexdata.com/v4/payloads").then((res) => {
+    console.log(res.data.flight_number);
+    setrocketName(res.data.dragon);
+  })
 
   }, []);
   //fetch("https://api.spacexdata.com/v4/rockets").then((res) => res.json()).then((data) => {console.log(data)})
   //setrocketName(res.data.name);
-  Axios.get("https://api.spacexdata.com/v4/payloads").then((res) => {
-    console.log(res.data);
-    setrocketName(res.data.name);
-  })
+  
 
   return (
     <div className="App">
