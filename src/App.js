833d871df94b4232b5a27885a6430a3179dabcdb
Parth Sharma <pxs4528@mavs.uta.edu>
2022-10-01 23:18:48 -0500

    LOLLOLO

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
 
+fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) => {console.log(data)})
 function App() {
+  fetch("https://api.spacexdata.com/v4/rockets").then((res) => res.json()).then((data) => {console.log(data)})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Generate rocket for me!</button>
+      <p></p>
     </div>
   );
 }
